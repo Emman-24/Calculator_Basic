4c977db0df727cb42b1c90dcb6db0d22b9d73810
Emman <emmanuel240158@gmail.com>
2023-11-05 20:29:54 -0500

    Stage 1/4: The layout

--- app/src/main/java/com/example/calculatorbasic/view/MainActivity.kt
+++ app/src/main/java/com/example/calculatorbasic/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.calculatorbasic
+package com.example.calculatorbasic.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.calculatorbasic.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
